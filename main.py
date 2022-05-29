e2b7231173e99ed10cff2a3f06ccb07331b44e8b
Tekky <98614666+xtekky@users.noreply.github.com>
2022-05-29 21:23:37 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests, threading, random
+
+
+class Checker:
+    def __init__(self):
+        self.threads = 10
+        self.hits    = 0
+        self.fails   = 0
+        self.file    = open('hits.txt', 'a')
+        
+        while True:
+            if threading.active_count() < self.threads:
+                threading.Thread(target=self.check_email)
+
+    def check_email(self):
+        email = f"{''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=5))}@gmail.com"
+        r = requests.get(f"https://mail.google.com/mail/gxlu?email={email}")
+        try:
+            if r.headers["Set-Cookie"]:
+                self.fails += 1
+    
+            else:
+                self.hits += 1
+                print(email, file=self.file)
+        except:
+            self.hits += 1
+            print(email, file=self.file)
+
+
+Checker()
